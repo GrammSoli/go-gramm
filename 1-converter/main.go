dc8d849d0c0c75395fe28edf83615cd0266b3bf0
GrammSoli <u-698@yandex.ru>
2025-06-08 21:27:43 +0500

    Функция считывания ввода от пользователя. Пустая функция расчета

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
-const (
-	usdToEur = 0.88
-	usdToRub = 79.13
-	eurToRub = usdToRub / usdToEur
-)
-
 func main() {
-
 	fmt.Println("1 EUR в RUB")
-
+	usdToEur, usdToRub := getUserInput()
+	eurToRub := (usdToRub / usdToEur)
 	fmt.Print(eurToRub)
+}
+
+func getUserInput() (float64, float64) {
+	var usdToEur float64
+	var usdToRub float64
+	fmt.Print("Введите курс USD в EUR: ")
+	fmt.Scan(&usdToEur)
+	fmt.Print("Ведите курс USD в RUB: ")
+	fmt.Scan(&usdToRub)
+	return usdToEur, usdToRub
+}
+
+func currency(number int, usd float64, eur float64) float64 {
+
 }
